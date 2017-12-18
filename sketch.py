8c1b9e3646030fda6e67d4e3ae0b283a85d98b81
Arthur Ngondo <ngondoarthur@gmail.com>
2017-12-18 20:51:54 +0300

    Process data in the file

--- sketch.py
+++ sketch.py
@@ -4,3 +4,10 @@ A play on files and exceptions.
     * Reading data from files
     * Exception handling
 """
+
+# Init
+the_file = open('sketch.txt') # Open file and assign object
+
+print(the_file.readlines(), end='') # Print data in the file as list
+
+the_file.close() # Close file object
