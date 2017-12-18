5f41acafcc51a2a24a37dd880743623eaf4c3e60
Arthur Ngondo <ngondoarthur@gmail.com>
2017-12-18 21:16:11 +0300

    Handle ValueError: need more than 1 value to unpack when split() encounters a line without the specified separator - fragile-fix

--- sketch.py
+++ sketch.py
@@ -11,9 +11,10 @@ the_file = open('sketch.txt') # Open file and assign object
 # Process data, extract each part from each line and display on screen
 
 for each_line in the_file:
-    (role, line_spoken) = each_line.split(':', 1)
-    print(role, end='')
-    print(' SAID: ', end='')
-    print(line_spoken, end='')
+    if not each_line.find(':') == -1:
+        (role, line_spoken) = each_line.split(':', 1)
+        print(role, end='')
+        print(' SAID: ', end='')
+        print(line_spoken, end='')
 
 the_file.close() # Close file object
