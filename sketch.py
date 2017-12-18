d9327e0dd029605c14f12d360a73f51180a12e23
Arthur Ngondo <ngondoarthur@gmail.com>
2017-12-18 21:42:45 +0300

    Handle FileNotFoundError: [Errno 2] No such file or directory:  - prints error msg

--- sketch.py
+++ sketch.py
@@ -5,21 +5,25 @@ A play on files and exceptions.
     * Exception handling
 """
 
-# Init
-the_file = open('sketch.txt') # Open file and assign object
+try: # Check that file exists
+    # Init
+    the_file = open('sketch.txt') # Open file and assign object
 
-# Process data, extract each part from each line and display on screen
+    # Process data, extract each part from each line and display on screen
 
-for each_line in the_file:
-   # Skip lines that dont contain separator
-    try:
-        (role, line_spoken) = each_line.split(':', 1)
-        print(role, end='')
-        print(' SAID: ', end='')
-        print(line_spoken, end='')
-    
-    except ValueError as err:
-        print(err)
+    for each_line in the_file:
+    # Skip lines that dont contain separator
+        try:
+            (role, line_spoken) = each_line.split(':', 1)
+            print(role, end='')
+            print(' SAID: ', end='')
+            print(line_spoken, end='')
         
+        except ValueError as data_err:
+            print(data_err)
+            
 
-the_file.close() # Close file object
+    the_file.close() # Close file object
+
+except IOError as no_file_err:
+    print(no_file_err)
