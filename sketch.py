04505583608f302ad8ddb52b6db35be6fddcf4eb
Arthur Ngondo <ngondoarthur@gmail.com>
2017-12-19 09:23:00 +0300

    Rewrite code to use 'with' statement to handle the closing of a potentially opened data file

--- sketch.py
+++ sketch.py
@@ -12,42 +12,35 @@ man = []
 other_man = []
 
 try: # Check that file exists
-    # Init
-    the_file = open('sketch.txt') # Open file and assign object
-
-    # Process data, extract each part from each line, append to list and 
-    # save to appropriate file
-
-    for each_line in the_file:
-    # Skip lines that dont contain separator
-        try:
-            (role, line_spoken) = each_line.split(':', 1)
-            line_spoken = line_spoken.strip()
-            if role == 'Man':
-                man.append(line_spoken)
-            elif role == 'Other Man':
-                other_man.append(line_spoken)
-        
-        except ValueError as data_err:
-            print(data_err, ': Line skipped (ignored)')
-            print('No separator (:) in Line ->',each_line)
+    # Init: Open file and assign object
+    with open('sketch.txt') as the_file:
+        # Process data, extract each part from each line, append to list and 
+        # save to appropriate file
+        for each_line in the_file:
+        # Skip lines that dont contain separator
+            try:
+                (role, line_spoken) = each_line.split(':', 1)
+                line_spoken = line_spoken.strip()
+                if role == 'Man':
+                    man.append(line_spoken)
+                elif role == 'Other Man':
+                    other_man.append(line_spoken)
             
-    the_file.close() # Close file object
-
+            except ValueError as data_err:
+                print(data_err, ': Line skipped (ignored)')
+                print('No separator (:) in Line ->',each_line)
+                
 except IOError as no_file_err:
     print(no_file_err)
 
 try:
     # Open output data files in write mode, assign to file objects
-    man_file = open('man_data.txt', 'w')
-    other_man_file = open('other_man_data.txt', 'w')
-    # Write to disk files
-    print(man, file=man_file)
-    print(other_man, file=other_man_file)
-
+    with open('man_data.txt', 'w') as man_file, \
+    open('other_man_data.txt', 'w') as other_man_file:
+        # Write to disk files
+        print(man, file=man_file)   
+        print(other_man, file=other_man_file)
+    
 except IOError as file_err:
     print('File Error', file_err)
 
-finally:
-    man_file.close()
-    other_man_file.close()
