ab2dfeb02d702a91afcaa3997fa2cdfb7a8b25f9
Arthur Ngondo <ngondoarthur@gmail.com>
2017-12-19 13:37:39 +0300

    Pickle data from list

--- sketch.py
+++ sketch.py
@@ -5,8 +5,10 @@ A play on files and exceptions.
     * Exception handling
 Persistance
     * Saving data to files
+    * Pickling data
 """
-from scripts.nester import nester
+# from scripts.nester import nester
+import pickle
 
 # Init
 man = []
@@ -34,14 +36,23 @@ try: # Check that file exists
 except IOError as no_file_err:
     print(no_file_err)
 
-try:
-    # Open output data files in write mode, assign to file objects
-    with open('text/man_data.txt', 'w') as man_file, \
-    open('text/other_man_data.txt', 'w') as other_man_file:
-        # Write to disk files in readable format
-        nester(man, data_output=man_file)
-        nester(other_man, data_output=other_man_file)  
+# try:
+#     # Open output data files in write mode, assign to file objects
+#     with open('text/man_data.txt', 'w') as man_file, \
+#     open('text/other_man_data.txt', 'w') as other_man_file:
+#         # Write to disk files in readable format
+#         nester(man, data_output=man_file)
+#         nester(other_man, data_output=other_man_file)  
     
-except IOError as file_err:
-    print('File Error', file_err)
+# except IOError as file_err:
+#     print('File Error', file_err)
 
+# Save with dump and restore with load
+try:
+    with open('static/man_data.pickle', 'wb') as man_file, \
+        open('static/other_man_data.pickle', 'wb') as other_man_file: # Access mode "writable, binary"
+        pickle.dump(man, man_file)
+        pickle.dump(other_man, other_man_file)
+        
+except pickle.PickleError as perr:
+    print('Ooops...', perr)
